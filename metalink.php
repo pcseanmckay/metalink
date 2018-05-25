43f5c3810a0a27fde4f6ce07f1e541139952afd7
pcseanmckay <pc.seanmckay@gmail.com>
2018-05-25 15:39:40 -0500

    Updated php script to loop through file.
    The script loops through the path.txt file and finds a match.  When a
    match is found, it echos the xml file contents and ends.

--- metalink.php
+++ metalink.php
@@ -18,55 +18,25 @@ while(!feof($myfile)) {
   #$key = $line_array[0] . "|" .$line_array[1];
   #$value = $line_array[2];
   #array_push($paths, $key=>$value);
+  $myrepo = $line_array[0];
+  $myarch = $line_array[1];
+  $mypath = $line_array[2];
+
+  if ($myrepo == $repo and $myarch == $arch) {
+    echo '<metalink version="3.0" type="dynamic" pubdate="Tue, 22 May 2018 15:28:39 GMT" generator="mirrormanager">
+    <files>
+        <file name="repomd.xml">'
+            #<mm0:timestamp>1509861107</mm0:timestamp>
+            #<size>3926</size>
+             . '<resources maxconnections="1">
+                <url protocol="http" type="http" location="US" preference="100">http://mirror.rackspace.com' . $mypath . '</url>
+            </resources>
+        </file>
+    </files>
+</metalink>';
+  break;
+  }
 }
 fclose($myfile);
-
-if (strncmp($repo, "fedora-", 7) == 0) {
-    $url = explode("-", $repo);
-    $ver = $url[1];
-    echo '<metalink version="3.0" type="dynamic" pubdate="Tue, 22 May 2018 15:28:39 GMT" generator="mirrormanager">
-    <files>
-        <file name="repomd.xml">'
-            #<mm0:timestamp>1509861107</mm0:timestamp>
-            #<size>3926</size>
-             . '<resources maxconnections="1">
-                <url protocol="http" type="http" location="US" preference="100">http://mirror.rackspace.com/fedora/releases/' . $ver . '/Everything/' . $arch . '/os/repodata/repomd.xml</url>
-            </resources>
-        </file>
-    </files>
-</metalink>';
-}
-elseif (strncmp($repo, "updates", 7) == 0) {
-    $url = explode("-", $repo);
-    $tmpver = $url[2];
-    $ver = substr($tmpver, 1);
-    echo '<metalink version="3.0" type="dynamic" pubdate="Tue, 22 May 2018 15:28:39 GMT" generator="mirrormanager">
-    <files>
-        <file name="repomd.xml">'
-            #<mm0:timestamp>1509861107</mm0:timestamp>
-            #<size>3926</size>
-             . '<resources maxconnections="1">
-                <url protocol="http" type="http" location="US" preference="100">http://mirror.rackspace.com/fedora/updates/' . $ver . '/' . $arch . '/repodata/repomd.xml</url>
-            </resources>
-        </file>
-    </files>
-</metalink>';
-}
-elseif (strncmp($repo, "epel-", 5) == 0) {
-    $url = explode("-", $repo);
-    $ver = $url[1];
-    echo '<metalink version="3.0" type="dynamic" pubdate="Tue, 22 May 2018 15:28:39 GMT" generator="mirrormanager">
-    <files>
-        <file name="repomd.xml">'
-            #<mm0:timestamp>1509861107</mm0:timestamp>
-            #<size>3926</size>
-             . '<resources maxconnections="1">
-                <url protocol="http" type="http" location="US" preference="100">http://mirror.rackspace.com/epel/' . $ver . '/' . $arch . '/repodata/repomd.xml</url>
-            </resources>
-        </file>
-    </files>
-</metalink>';
-
-}
 
 ?>
